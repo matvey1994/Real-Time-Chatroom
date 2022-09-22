8b76da4be4bd9d420e97fcaada58590480a3fcec
Matvey Raevskiy <80818461+matvey1994@users.noreply.github.com>
2022-09-22 18:44:19 +0800

    Adding documents via event listener

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './styles.css';
 
 // Import the functions you need from the SDKs required
 import { initializeApp } from 'firebase/app'
-import { getFirestore, collection, getDocs } from 'firebase/firestore'
+import { getFirestore, collection, getDocs, addDoc, Timestamp } from 'firebase/firestore'
 
 
 // Web App's Firebase configuration
@@ -32,16 +32,60 @@ getDocs(colRef)
         snapshot.docs.forEach((doc) => {
             message.push({ 
                  ...doc.data(),
-                 id: doc.id 
+                 id: doc.id
             })
         })
-        
+        console.log(message)
     })
     .catch(err => {
         console.log(err.message)
     })
 
-console.log(colRef)
+
+const addChatForm = document.querySelector('.new-chat')
+addChatForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    const now = new Date()
+
+    addDoc(colRef, {
+        message: addChatForm.message.value,
+        created_at: Timestamp.fromDate(now)
+
+    })
+    .then(() => {
+        addChatForm.reset()
+    })
+})
+
+// class Chatroom {
+//     constructor(room, username) {
+//         this.room = room;
+//         this.username = username;
+//         this.chats = colRef;
+//     }
+//     // async addChat(message){
+//     //     // Format a chat object
+//     //     const now = new Date();
+//     //     const chat = {
+//     //         message,
+//     //         username: this.username,
+//     //         room: this.room,
+//     //         created_at: Timestamp.fromDate(now)
+//     //     };
+//     //     // Save the chat document
+//     //     const response = await this.chats.add(chat);
+//     //     return response
+//     // } 
+// }
+
+// const chatroom = new Chatroom('gaming', 'matvey')
+// console.log(chatroom)
+// chatroom.addChat('hello everyone')
+//     .then(() => console.log('chat added'))
+//     .catch(err => console.log(err))
+
+
 
     
     
